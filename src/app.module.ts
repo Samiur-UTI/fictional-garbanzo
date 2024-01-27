120c38906d436f561521c8bce542367cee0fe387
samiur <samiur@synesisit.info>
2024-01-27 21:27:37 +0600

    feat(entities):initialized user and profile entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 @Module({
   imports: [UserModule, AuthModule, CommonModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/model/entity/user.entity.ts
@@ -0,0 +1,21 @@
+// src/model/entity/user.entity.ts
+
+import { Entity, Column, OneToOne, JoinColumn } from 'typeorm';
+import { Profile } from './profile.entity';
+import { BaseEntity } from './base.entity';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @OneToOne(() => Profile, (profile) => profile.user, {
+    eager: true,
+    cascade: true,
+  })
+  @JoinColumn()
+  profile: Profile;
+}
