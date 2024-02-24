654307ca49ef077d3d60dde3551d08bcb2850176
samiur <samiur@synesisit.info>
2024-02-24 20:47:36 +0600

    feat(app-name):updated app name

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -20,47 +20,47 @@ import { AuthGuard } from 'src/auth/jwt-auth.guard';
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
-// Run this once after installing application
-//   @Get('seed-platforms')
-//   async seedPlatforms(@Res() res: Response) {
-//     try {
-//       // Define the platforms to seed
-//       const platformsToSeed: Partial<Platform>[] = [
-//         { platformName: 'YouTube' },
-//         { platformName: 'Facebook' },
-//         { platformName: 'Instagram' },
-//         { platformName: 'TikTok' },
-//       ];
+  // Run this once after installing application
+  //   @Get('seed-platforms')
+  //   async seedPlatforms(@Res() res: Response) {
+  //     try {
+  //       // Define the platforms to seed
+  //       const platformsToSeed: Partial<Platform>[] = [
+  //         { platformName: 'YouTube' },
+  //         { platformName: 'Facebook' },
+  //         { platformName: 'Instagram' },
+  //         { platformName: 'TikTok' },
+  //       ];
 
-//       // Create platforms in the database
-//       await Promise.all(
-//         platformsToSeed.map((platform) =>
-//           this.accountService.createPlatform(platform),
-//         ),
-//       );
+  //       // Create platforms in the database
+  //       await Promise.all(
+  //         platformsToSeed.map((platform) =>
+  //           this.accountService.createPlatform(platform),
+  //         ),
+  //       );
 
-//       return res
-//         .status(HttpStatus.CREATED)
-//         .json({ message: 'Platforms seeded successfully' });
-//     } catch (error) {
-//         console.log(error)
-//       return res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
-//     }
-//   }
+  //       return res
+  //         .status(HttpStatus.CREATED)
+  //         .json({ message: 'Platforms seeded successfully' });
+  //     } catch (error) {
+  //         console.log(error)
+  //       return res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
+  //     }
+  //   }
 
-    @UseGuards(AuthGuard)
-    @Post('add-account')
-    async createAccount(
-      @Body() createAccountDto: CreateAccountDto,
-      @Res() res: Response,
-    ) {
-      try {
-        await this.accountService.createAccount(createAccountDto);
-        return res
-          .status(HttpStatus.CREATED)
-          .json({ message: 'Successfully created account' });
-      } catch (error) {
-        return res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
-      }
+  @UseGuards(AuthGuard)
+  @Post('add-account')
+  async createAccount(
+    @Body() createAccountDto: CreateAccountDto,
+    @Res() res: Response,
+  ) {
+    try {
+      await this.accountService.createAccount(createAccountDto);
+      return res
+        .status(HttpStatus.CREATED)
+        .json({ message: 'Successfully created account' });
+    } catch (error) {
+      return res.status(HttpStatus.BAD_REQUEST).json({ error: error.message });
     }
+  }
 }
